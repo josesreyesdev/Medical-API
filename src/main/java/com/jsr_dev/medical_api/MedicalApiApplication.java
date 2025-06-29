1d465e365a585dff2fa09f29db5cc63524eb1008
josesreyesdev <josesanchez09190@gmail.com>
2025-06-29 00:48:04 -0600

    Added project

---
+++ src/main/java/com/jsr_dev/medical_api/MedicalApiApplication.java
@@ -0,0 +1,13 @@
+package com.jsr_dev.medical_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalApiApplication.class, args);
+	}
+
+}
