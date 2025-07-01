1c2675f91220bdd162e9e552a0b6f5e73577388e
josesreyesdev <josesanchez09190@gmail.com>
2025-07-01 02:55:16 -0600

    Added Patient Controller

---
+++ src/main/java/com/jsr_dev/medical_api/controller/PatientController.java
@@ -0,0 +1,16 @@
+package com.jsr_dev.medical_api.controller;
+
+import com.jsr_dev.medical_api.patient.PatientRequest;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/patients")
+public class PatientController {
+    @PostMapping
+    public void addPatient(@RequestBody PatientRequest patientRequest) {
+        System.out.println(patientRequest);
+    }
+}
---
+++ src/main/java/com/jsr_dev/medical_api/patient/PatientRequest.java
@@ -0,0 +1,12 @@
+package com.jsr_dev.medical_api.patient;
+
+import com.jsr_dev.medical_api.address.AddressRequest;
+
+public record PatientRequest(
+        String name,
+        String email,
+        String identityDocument,
+        String phone,
+        AddressRequest addressRequest
+) {
+}
