aa321235aae14235b2fcf36695ccc113b149f7ac
josesreyesdev <josesanchez09190@gmail.com>
2025-07-03 21:57:49 -0600

    added patient entity

---
+++ src/main/java/com/jsr_dev/medical_api/patient/Patient.java
@@ -0,0 +1,25 @@
+package com.jsr_dev.medical_api.patient;
+
+import com.jsr_dev.medical_api.address.Address;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Table(name = "patients")
+@Entity(name = "Patient")
+public class Patient {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String identityDocument;
+    private String phoneNumber;
+    @Embedded
+    private Address address;
+}
