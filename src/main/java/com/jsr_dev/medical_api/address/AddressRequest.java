449938672b53ca6047193eac1814981a9cd71097
josesreyesdev <josesanchez09190@gmail.com>
2025-07-06 00:31:33 -0600

    added bean validations in address request

--- src/main/java/com/jsr_dev/medical_api/address/AddressRequest.java
+++ src/main/java/com/jsr_dev/medical_api/address/AddressRequest.java
@@ -1,13 +1,31 @@
 package com.jsr_dev.medical_api.address;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
 public record AddressRequest(
+        @NotBlank(message = "Street must not be empty.")
         String street,
+
+        @NotBlank(message = "State or province must not be empty.")
         String stateOrProvince,
+
+        @NotBlank(message = "Municipality or delegation must not be empty.")
         String municipalityOrDelegation,
+
+        @NotBlank(message = "City must not be empty.")
         String city,
+
+        @NotBlank (message = "Zip code must not be empty.")
+        @Pattern(regexp = "\\d{4,8}", message = "Zip code must contain between 4 and 8 digits.")
         String zipCode,
+
+        @NotBlank(message = "Country must not be empty.")
         String country,
+
+        @NotBlank(message = "External number must not be empty.")
         String externalNumber,
+
         String internalNumber,
         String complement
 ) {
