eba91419cbe4545bd50725cd834bb01a4e41cbeb
josesreyesdev <josesanchez09190@gmail.com>
2025-07-07 01:42:49 -0600

    added patient mapper to map patient response

--- src/main/java/com/jsr_dev/medical_api/controller/PatientController.java
+++ src/main/java/com/jsr_dev/medical_api/controller/PatientController.java
@@ -3,12 +3,13 @@ package com.jsr_dev.medical_api.controller;
 import com.jsr_dev.medical_api.patient.PatientMapper;
 import com.jsr_dev.medical_api.patient.PatientRepository;
 import com.jsr_dev.medical_api.patient.PatientRequest;
+import com.jsr_dev.medical_api.patient.PatientResponse;
 import jakarta.validation.Valid;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/patients")
@@ -24,5 +25,12 @@ public class PatientController {
     @PostMapping
     public void addPatient(@RequestBody @Valid PatientRequest patientRequest) {
         repository.save(PatientMapper.mapToPatient(patientRequest));
+    }
+
+    @GetMapping
+    public List<PatientResponse> getAllPatients() {
+        return repository.findAll().stream()
+                .map(PatientMapper::mapToPatientResponse)
+                .collect(Collectors.toList());
     }
 }
