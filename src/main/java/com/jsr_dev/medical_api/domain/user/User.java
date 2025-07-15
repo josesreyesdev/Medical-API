d8c7fed8ffaa8409a0b95e903344f990de1360eb
josesreyesdev <josesanchez09190@gmail.com>
2025-07-15 00:17:57 -0600

    added user details

--- src/main/java/com/jsr_dev/medical_api/domain/user/User.java
+++ src/main/java/com/jsr_dev/medical_api/domain/user/User.java
@@ -6,8 +6,13 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Table(name = "users")
 @Entity(name = "User")
@@ -15,7 +20,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
@@ -31,4 +36,41 @@ public class User {
             inverseJoinColumns = @JoinColumn(name = "profile_id")
     )
     private List<Profile> profiles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return profiles.stream()
+                .map(p -> new SimpleGrantedAuthority("ROLE_" + p.getName().toUpperCase()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
