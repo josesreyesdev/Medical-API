026ce24029e6fea18b6086c66beaa6d3e203d7b8
josesreyesdev <josesanchez09190@gmail.com>
2025-07-17 03:10:35 -0600

    added retrieve token in security filter

--- src/main/java/com/jsr_dev/medical_api/infra/security/SecurityFilter.java
+++ src/main/java/com/jsr_dev/medical_api/infra/security/SecurityFilter.java
@@ -12,9 +12,32 @@ import java.io.IOException;
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
 
-
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        String authHeader = request.getHeader("Authorization");
+
+        if (doesNotContainBearerToken(authHeader)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String jwtToken = extractTokenValue(authHeader);
+        String email = "";
+
         filterChain.doFilter(request, response);
     }
+
+    private String extractTokenValue(String header) {
+        return header.substring("Bearer ".length());
+    }
+
+    private boolean doesNotContainBearerToken(String header) {
+        return header == null || !header.startsWith("Bearer ");
+    }
+
 }
