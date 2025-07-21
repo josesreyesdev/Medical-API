25b368278ba1a63e69354f45dd9e2eb643253a81
josesreyesdev <josesanchez09190@gmail.com>
2025-07-20 23:44:38 -0600

    added request and response appointment

---
+++ src/main/java/com/jsr_dev/medical_api/domain/appointment/AddAppointmentRequest.java
@@ -0,0 +1,21 @@
+package com.jsr_dev.medical_api.domain.appointment;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+
+public record AddAppointmentRequest(
+        @JsonAlias({"idPatient", "id_patient"})
+        @NotNull(message = "is required")
+        Long idPatient,
+
+        @JsonAlias({"idPhysician", "id_physician"})
+        Long idMedic,
+
+        @NotNull(message = "is required")
+        @Future(message = "must be in the future")
+        LocalDateTime date
+) {
+}
---
+++ src/main/java/com/jsr_dev/medical_api/domain/appointment/AppointmentResponse.java
@@ -0,0 +1,11 @@
+package com.jsr_dev.medical_api.domain.appointment;
+
+import java.time.LocalDateTime;
+
+public record AppointmentResponse(
+        Long id,
+        Long idPatient,
+        Long idPhysician,
+        LocalDateTime date
+) {
+}
