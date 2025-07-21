52afb9eaccaae1c4106418f7759ee20160bb38be
josesreyesdev <josesanchez09190@gmail.com>
2025-07-20 23:45:12 -0600

    added appointment controller

---
+++ src/main/java/com/jsr_dev/medical_api/controller/AppointmentController.java
@@ -0,0 +1,22 @@
+package com.jsr_dev.medical_api.controller;
+
+import com.jsr_dev.medical_api.domain.appointment.AddAppointmentRequest;
+import com.jsr_dev.medical_api.domain.appointment.AppointmentResponse;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/consultations")
+public class AppointmentController {
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity<AppointmentResponse> add(@RequestBody @Valid AddAppointmentRequest addConsultation) {
+        return ResponseEntity.ok(new AppointmentResponse(null, null, null, null));
+    }
+}
