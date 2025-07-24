6841fe049a6b768ad9947d880868f1fb5f3c82f9
josesreyesdev <josesanchez09190@gmail.com>
2025-07-24 03:53:20 -0600

    added anticipation appointment validator

---
+++ src/main/java/com/jsr_dev/medical_api/domain/appointment/validations/reserve/AppointmentAnticipationValidator.java
@@ -0,0 +1,23 @@
+package com.jsr_dev.medical_api.domain.appointment.validations.reserve;
+
+import com.jsr_dev.medical_api.domain.appointment.AddAppointmentRequest;
+import com.jsr_dev.medical_api.infra.exceptions.IntegrityValidationException;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public class AppointmentAnticipationValidator implements AppointmentValidator{
+    /*
+     * The appointment reservation, it cannot be less than 30 minutes.
+     * */
+    @Override
+    public void validate(AddAppointmentRequest data) {
+        LocalDateTime appointmentDate = data.date();
+        LocalDateTime currentTime = LocalDateTime.now();
+        long differenceInMinutes = Duration.between(currentTime, appointmentDate).toMinutes();
+
+        if (differenceInMinutes < 30) {
+            throw new IntegrityValidationException("The appointment reservation, it cannot be less than 30 minutes.");
+        }
+    }
+}
