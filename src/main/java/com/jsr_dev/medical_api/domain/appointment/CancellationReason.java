58b16d0fe44669796bf577e8ff83c6cb4f0ecc1c
josesreyesdev <josesanchez09190@gmail.com>
2025-07-26 04:14:20 -0600

    upgraded enum cancellation reason

--- src/main/java/com/jsr_dev/medical_api/domain/appointment/CancellationReason.java
+++ src/main/java/com/jsr_dev/medical_api/domain/appointment/CancellationReason.java
@@ -1,12 +1,23 @@
 package com.jsr_dev.medical_api.domain.appointment;
 
 import java.text.Normalizer;
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public enum CancellationReason {
     PATIENT_DESISTED("Patient desisted", "Paciente desistió"),
     PHYSICIAN_CANCELLATION("Physician desisted", "Médico canceló"),
     OTHER("Other", "Otro");
+
+    private static final Map<String, CancellationReason> ENG_REASON_MAP = new HashMap<>();
+    private static final Map<String, CancellationReason> ESP_REASON_MAP = new HashMap<>();
+
+    static {
+        for (CancellationReason reason : values()) {
+            ENG_REASON_MAP.put(normalize(reason.engCancellationReason), reason);
+            ESP_REASON_MAP.put(normalize(reason.espCancellationReason), reason);
+        }
+    }
 
     private final String engCancellationReason;
     private final String espCancellationReason;
@@ -17,18 +28,11 @@ public enum CancellationReason {
     }
 
     public static CancellationReason fromEng(String text) {
-        return Arrays.stream(values())
-                .filter(g -> g.engCancellationReason.equalsIgnoreCase(text))
-                .findFirst()
-                .orElse(null);
+        return ENG_REASON_MAP.get(normalize(text));
     }
 
     public static CancellationReason fromEsp(String text) {
-        String normalizedText = normalize(text);
-        return Arrays.stream(values())
-                .filter(g -> normalize(g.espCancellationReason).equalsIgnoreCase(normalizedText))
-                .findFirst()
-                .orElse(null);
+        return ESP_REASON_MAP.get(normalize(text));
     }
 
     public static CancellationReason parseCancellationReason(String cancellationReason) {
