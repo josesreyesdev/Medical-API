f3cebf098839aaa2b1de1fecccfbaf2cad9bfa20
josesreyesdev <josesanchez09190@gmail.com>
2025-07-28 02:32:35 -0600

    added database configuration

---
+++ src/test/java/com/jsr_dev/medical_api/domain/physician/PhysicianRepositoryTest.java
@@ -0,0 +1,18 @@
+package com.jsr_dev.medical_api.domain.physician;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+class PhysicianRepositoryTest {
+
+    @Test
+    void chooseARandomPhysicianAvailableOnTheDate() {
+    }
+}
