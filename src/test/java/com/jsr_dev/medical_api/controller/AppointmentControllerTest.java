2216660a6b04cf38867fef9db181a628f1a0d242
josesreyesdev <josesanchez09190@gmail.com>
2025-07-28 23:36:18 -0600

    added return http 400 (Bad request) when the request has no data

---
+++ src/test/java/com/jsr_dev/medical_api/controller/AppointmentControllerTest.java
@@ -0,0 +1,33 @@
+package com.jsr_dev.medical_api.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AppointmentControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("Returns HTTP 400 (BAD REQUEST) when the request has no data.")
+    @WithMockUser // Skip authentication configuration for requests
+    void addScenario1() throws Exception {
+        MockHttpServletResponse response = mockMvc.perform(post("/appointments"))
+                .andReturn().getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+}
